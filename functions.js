243929cd4771516672f9c8c4238e405b30c87659
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2023-03-01 21:34:50 -0600

    added functions

---
+++ functions.js
@@ -0,0 +1,16 @@
+
+
+
+function sum(num1,num2){
+    return num1 + num2
+}
+
+function sayHello(){
+    alert("Hello")
+    console.log("Hello");
+}
+
+
+
+let div1 = document.getElementById("div1");
+div1.addEventListener("click", sayHello);
