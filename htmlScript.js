b1c20310053b20e350827a035865a51b676276df
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2023-03-03 10:29:31 -0600

    feat: embedded youtube video

--- htmlScript.js
+++ htmlScript.js
@@ -1,6 +1,21 @@
 import {currentDate, sayHello} from "./functions.js";
 
 
+let youtubeVideo = document.getElementById("video");
+youtubeVideo.setAttribute("src","https://www.youtube.com/embed/HdiKqg23D0o?controls=0" );
+youtubeVideo.setAttribute("title","YouTube video player");
+youtubeVideo.setAttribute("width","560");
+youtubeVideo.setAttribute("height","315");
+youtubeVideo.setAttribute("frameBorder","0");
+youtubeVideo.setAttribute("allow","accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share");
+
+
+
+
+
+
+
+
 
 
 let div1 = document.getElementById("div1");
@@ -10,3 +25,4 @@ let dateBtn = document.getElementById("dateButton");
 dateBtn.addEventListener("click", function (){
     alert(currentDate())
 })
+
