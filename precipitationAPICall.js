c3b68c8ccea9f38c3a8a9a5035310891ca11c086
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2023-03-29 12:59:22 -0500

    feat: changed color and positioning structure of time and location

--- precipitationAPICall.js
+++ precipitationAPICall.js
@@ -12,12 +12,13 @@ let key = OPENWEATHER_API_KEY
 let layer = "precipitation_new"
 let z = 1
 let x = 1
-let y = 2
+let y = 1
 // let units = "imperial"
 // let lang = "en"
 
-let url = `https://tile.openweathermap.org/map/${layer}/${z}/${x}/${y}.png?appid=${key}`
+// let url = `https://tile.openweathermap.org/map/${layer}/${z}/${x}/${y}.png?appid=${key}`
 // let url = `https://maps.openweathermap.org/maps/2.0/weather/1h/HRD0/4/1/6?&appid=${key}`
+let url = `http://maps.openweathermap.org/maps/2.0/weather/${layer}/${z}/${x}/${y}?appid=${key}`
 // let url = `https://api.openweathermap.org/data/2.5/weather?q=${city name},${country code}&appid=${key}`
 fetch(url)
     .then(resp => {
