1102422a60a7581d872788d27623a6fef4f7442a
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2023-03-30 23:07:56 -0500

    feat: added google maps API call

---
+++ googleMapsAPICall.js
@@ -0,0 +1,18 @@
+import { Loader } from "@googlemaps/js-api-loader"
+
+
+let map;
+const loader = new Loader({
+    apiKey: GOOGLEMAPS_API_KEY ,
+    version: "weekly",
+    // ...additionalOptions,
+});
+
+loader.load().then(async () => {
+    const { Map } = await google.maps.importLibrary("maps");
+
+    map = new Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
+});
