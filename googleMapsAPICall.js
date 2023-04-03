d0a238e807e9b9efd345cd642c8ba7e31dc80716
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2023-04-03 12:32:03 -0500

    feat: enabled users location to set center of google maps api call

--- googleMapsAPICall.js
+++ googleMapsAPICall.js
@@ -1,36 +1,46 @@
-export {initMap};
-
-
-
-(g=>{
-    let h, a, k, p = "The Google Maps JavaScript API", c = "google", l = "importLibrary", q = "__ib__", m = document,
-        b = window;b=b[c]||(b[c]={});
-    const d = b.maps || (b.maps = {}), r = new Set, e = new URLSearchParams,
-        u = () => h || (h = new Promise(async (f, n) => {
-            await (a = m.createElement("script"));
-            e.set("libraries", [...r] + "");
-            for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]);
-            e.set("callback", c + ".maps." + q);
-            a.src = `https://maps.${c}apis.com/maps/api/js?` + e;
-            d[q] = f;
-            a.onerror = () => h = n(Error(p + " could not load."));
-            a.nonce = m.querySelector("script[nonce]")?.nonce || "";
-            m.head.append(a)
-        }));
-        d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f, ...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
-        ({key: GOOGLEMAPS_API_KEY, v: "weekly"});
 
 let map;
+let lat;
+let long;
 
 async function initMap() {
-    //@ts-ignore
-    const { Map } = await google.maps.importLibrary("maps");
 
-    map = new Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
-    });
+
+
+    navigator.geolocation.getCurrentPosition(async position => {
+
+        lat = parseFloat(position.coords.latitude.toFixed(2));
+        long = parseFloat(position.coords.longitude.toFixed(2));
+
+        (g=>{
+            let h, a, k, p = "The Google Maps JavaScript API", c = "google", l = "importLibrary", q = "__ib__", m = document,
+                b = window;b=b[c]||(b[c]={});
+            const d = b.maps || (b.maps = {}), r = new Set, e = new URLSearchParams,
+                u = () => h || (h = new Promise(async (f, n) => {
+                    await (a = m.createElement("script"));
+                    e.set("libraries", [...r] + "");
+                    for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]);
+                    e.set("callback", c + ".maps." + q);
+                    a.src = `https://maps.${c}apis.com/maps/api/js?` + e;
+                    d[q] = f;
+                    a.onerror = () => h = n(Error(p + " could not load."));
+                    a.nonce = m.querySelector("script[nonce]")?.nonce || "";
+                    m.head.append(a)
+                }));
+            d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f, ...n)=>r.add(f)&&u().then(()=>d[l](f,...n))
+        })
+        ({key: GOOGLEMAPS_API_KEY, v: "weekly"});
+        const {Map} = await google.maps.importLibrary("maps");
+
+        map = new Map(document.getElementById("map"), {
+            center: {lat: lat, lng: long},
+            // center: { lat: -34.397, lng: 150.644 },
+            zoom: 8,
+        });
+    })
 }
 
-//for some reason, when I do window.onload for initMap(), the city div just vanishes
+// for some reason, when I do window.onload for initMap(), the city div just vanishes
 initMap();
+
+
